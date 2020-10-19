a2be54e90579a0123febd6f5d382e5455543a01f
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-19 15:41:45 +0800

    [Word Frequency][John Carlos]refactor: rename list to distinctWordInfoList

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -27,12 +27,12 @@ public class WordFrequencyGame {
                 //get the map for the next step of sizing the same word
                 Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
 
-                List<WordInfo> list = new ArrayList<>();
+                List<WordInfo> distinctWordInfoList = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
                     WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
+                    distinctWordInfoList.add(wordInfo);
                 }
-                wordInfoList = list;
+                wordInfoList = distinctWordInfoList;
 
                 wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
