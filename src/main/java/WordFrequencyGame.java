0b566bb678c65e1601f1d210999a6d8d0ecc8783
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-19 15:40:07 +0800

    [Word Frequency][John Carlos]refactor: rename map to wordInfoMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -25,10 +25,10 @@ public class WordFrequencyGame {
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map =getListMap(wordInfoList);
+                Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
 
                 List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
+                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
                     WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
                     list.add(wordInfo);
                 }
